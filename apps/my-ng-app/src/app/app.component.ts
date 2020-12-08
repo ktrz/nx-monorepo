eae788ae4bae9e41b79a8c0bae3d0a9bf57d1411
Chris Trześniewski <krzysztof.trzesniewski@scalac.io>
2020-12-08 17:36:43 +0100

    Lib change

--- apps/my-ng-app/src/app/app.component.ts
+++ apps/my-ng-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { myLib } from '@nx-monorepo/my-lib';
 
 @Component({
   selector: 'nx-monorepo-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-ng-app';
+  mySubtitle = myLib();
 }
